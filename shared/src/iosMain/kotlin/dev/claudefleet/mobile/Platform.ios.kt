ca7152c19119b7bb49f6a66d172699cdb9d9c34f
martin-janci <2478078+martin-janci@users.noreply.github.com>
2026-09-21 21:11:49 +0200

    fix(ios): import the NSDate extension the epoch clock uses

--- shared/src/iosMain/kotlin/dev/claudefleet/mobile/Platform.ios.kt
+++ shared/src/iosMain/kotlin/dev/claudefleet/mobile/Platform.ios.kt
@@ -1,5 +1,8 @@
 package dev.claudefleet.mobile
 
+import platform.Foundation.NSDate
+import platform.Foundation.timeIntervalSince1970
+
 internal actual fun platformName(): String = "iOS"
 
-internal actual fun epochSeconds(): Long = platform.Foundation.NSDate().timeIntervalSince1970.toLong()
+internal actual fun epochSeconds(): Long = NSDate().timeIntervalSince1970.toLong()
